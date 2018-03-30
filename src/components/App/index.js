a263b7a584308a18262e6d1e384b0b610760eaba
Ceyhun <c.ashurbeyli@gmail.com>
2018-03-30 04:48:16 +0400

    translated chat design to html css in app component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 
+import './App.css';
+
 class App extends React.Component {
   render () {
     return (
       <div>
-        App works!
+        <div className="block-container">
+          <div className="messages-list">
+            <div className="messages-list__card">
+              <span className="messages-list__card-holder">Ninja</span>
+              <p>Great resource, thanks</p>
+              <span className="messages-list__datetime">10 Mar 2018 9:55</span>
+            </div>
+            <div className="clearfix" />
+            <div className="messages-list__card">
+              <span className="messages-list__card-holder">Ninja</span>
+              <p>Great resource, thanks</p>
+              <span className="messages-list__datetime">10 Mar 2018 9:55</span>
+            </div>
+            <div className="clearfix" />
+            <div className="messages-list__card">
+              <span className="messages-list__card-holder">Ninja</span>
+              <p>Great resource, thanks</p>
+              <span className="messages-list__datetime">10 Mar 2018 9:55</span>
+            </div>
+            <div className="clearfix" />
+            <div className="messages-list__card messages-list__card--right">
+              <p>Hey folks! I wanted to get in touch with you regarding the project.</p>
+              <span className="messages-list__datetime">10 Mar 2018 9:55</span>
+            </div>
+            <div className="clearfix" />
+          </div>
+        </div>
+        <div className="footer">
+          <div className="block-container">
+            <form className="send-form">
+              <input type="text" className="send-form__textInput" placeholder="Message" />
+              <button className="send-form__sendButton">Send</button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 
+import './styles.css';
+
 import App from './components/App';
 
 ReactDOM.render(
